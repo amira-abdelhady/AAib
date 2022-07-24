39426b0d975ea90231593abfc4eed4acbb1989fd
amira-abdelhady <amiraabdelhady885@gmail.com>
2022-07-24 10:22:19 +0200

    first commit from fixbug branch

--- src/link/link.js
+++ src/link/link.js
@@ -2,7 +2,13 @@
 const Link =(props)=>{
 console.log(props);
   return(
-    <a href={props.href}>{props.name}</a>
+    <>
+    <a href={props.href}>{props.students.name}</a>
+    <a href={props.href}>{props.students.degree}</a>
+    <a href={props.href}>{props.students.age}</a>
+    
+
+    </>
   )
 }
 
