5935356b2c38c74f1482ec98dfd22af793a38946
amira-abdelhady <37426262+amira-abdelhady@users.noreply.github.com>
2022-07-24 12:56:39 +0200

    comment all content

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import {useHistory, useLocation} from 'react-router-dom'
+// import React from 'react';
+// import {useHistory, useLocation} from 'react-router-dom'
 
-const Contact = (props) => {
+// const Contact = (props) => {
 
-    console.log(props);
+//     console.log(props);
         
-   const history= useHistory();
+//    const history= useHistory();
          
-  const location= useLocation()
-console.log(location);
-    const goToAbout=()=>{
+//   const location= useLocation()
+// console.log(location);
+//     const goToAbout=()=>{
 
-        // props.history.push("/about-us")
+//         // props.history.push("/about-us")
 
-        history.push("/about-us")
-    }
-    return (
-        <>
-         <div>
-            contact us
-        </div>
+//         history.push("/about-us")
+//     }
+//     return (
+//         <>
+//          <div>
+//             contact us
+//         </div>
 
-        <button onClick={()=>goToAbout()} className="btn btn-success">Go To About us</button>
+//         <button onClick={()=>goToAbout()} className="btn btn-success">Go To About us</button>
         
-        </>
+//         </>
        
-    );
-}
+//     );
+// }
 
-export default Contact;
+// export default Contact;
