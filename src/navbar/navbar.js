d330e72253db7d6ee751264884971ef29dbce547
amira-abdelhady <amiraabdelhady885@gmail.com>
2022-07-24 10:12:19 +0200

    adding to nav and link

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -58,6 +58,9 @@ changeName(){
           <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">link1</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">link2</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">link3</NavDropdown.Item>
           <NavDropdown.Divider />
           <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
         </NavDropdown>
